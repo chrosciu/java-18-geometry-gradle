f147c6bfebc1c33fe98690f80d536529359c5f61
Marcin Chrost <mchrost82@gmail.com>
2022-07-08 10:36:45 +0200

    workshop preparation

--- src/main/java/com/chrosciu/geometry/Polygon.java
+++
@@ -1,7 +0,0 @@
-package com.chrosciu.geometry;
-
-import java.util.Set;
-
-public abstract sealed class Polygon implements Shape permits Section, Square {
-    public abstract Set<Point> getVertices();
-}
